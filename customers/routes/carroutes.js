2acf3a287c41811c99c041d2967aef75c1248078
sanjayprasadF5 <sanjayprasad2321998@gmail.com>
2021-06-23 15:44:06 +0530

    changes in master

---
+++ customers/routes/carroutes.js
@@ -0,0 +1,8 @@
+// const carController = require("../controllers/carController");
+
+// const express = require("express");
+// const router = express.Router();
+
+// // router.get("/car", requireAuth, carController.get_cars);
+
+// module.exports = router;
